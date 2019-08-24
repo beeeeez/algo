09f266edf6a2739c8d58753ecd70847ee576f178
Brown <cibrown@labs.neit.edu>
2019-08-24 16:07:29 -0400

    fudge

--- recursion/src/recursion/look.java
+++ recursion/src/recursion/look.java
@@ -8,47 +8,26 @@ public class look {
 		
 	}
 	
-	public static String dotheThing(String jimmy,int iterations) {		
-		String jammy="";
-		char hold=jimmy.charAt(0);
-		int count=0;
-		for(int i=0;i<jimmy.length();i++) {
-			count++;
-			if(hold!=jimmy.charAt(i)) {			
-				jammy+=jimmy.charAt(i);
-				jammy+=count;
-				hold=jimmy.charAt(i);
-				count=0;
-			}
-			
-		}
-		iterations--;
+	public static String dotheThing(String jimmy,int iterations) {	
 		if(iterations==0) {
-			return jammy;
-		}
-		return dotheThing(jammy, iterations);
-	
-		
-		/*
-		if(i==0) {
 			return jimmy;
-		}		
-		for(int j=0;j<jimmy.length();j++) {
-			if(x==(int)jimmy.charAt(j)) {
-				c++;
-			}
-			else {
-				jammy+=((int)jimmy.charAt(j)-1);
-				jammy+=c;
-				x = (int)jimmy.charAt(j);
-			}
-			
 		}
+		StringBuilder result= new StringBuilder();
 		
-		
-		i--;
-		return dotheThing(jammy,i);
-	*/
-			
+		 
+		char repeat= jimmy.charAt(0);
+		jimmy= jimmy.substring(1) + " ";
+		int times= 1;	 
+		for(char actual: jimmy.toCharArray()){
+			if(actual != repeat){
+				result.append(times + "" + repeat);
+				times= 1;
+				repeat= actual;
+			}else{
+				times+= 1;
+			}
+		}
+		System.out.println(result.toString());
+		return dotheThing(result.toString(),iterations-1);
 	}
 }
